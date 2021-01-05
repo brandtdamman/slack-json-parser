53c7a3cd753232feca1dcb8db1cf0f31bfcade4b
Brandt Damman <brandtdamman@gmail.com>
2021-01-04 20:07:51 -0600

    Removed unnecessary abv. of Version.

--- src/python/slackjp.py
+++ src/python/slackjp.py
@@ -14,9 +14,10 @@ import os
 import sys
 
 # Argument Parser Setup
-_parser = argparse.ArgumentParser(description='Slack JSON Parser')
+_parser = argparse.ArgumentParser(description='Slack JSON Parser', epilog='Further \
+    information can be found via "man slackjp".')
 _parser.add_argument('-d', action='store_true', help='Download attached files.')
 _parser.add_argument('-D', action='store_true', nargs='+', help='')
 
-_parser.add_argument('-V', '--version', action='version', version='%(prog)s ' + _verNumber)
+_parser.add_argument('--version', action='version', version='%(prog)s ' + _verNumber)
 
